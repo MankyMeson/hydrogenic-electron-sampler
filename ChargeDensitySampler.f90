39b9b6c88dec4e1e93d9f94cb14864292efa52aa
MankyMeson <doylejohnson15@gmail.com>
2021-11-14 12:33:02 +0000

    Messed around with cartesian conversions

--- ChargeDensitySampler.f90
+++ ChargeDensitySampler.f90
@@ -1,10 +1,9 @@
 module sampler
   implicit none
   integer, parameter :: dp = kind(1.d0)
-! This contains all the functions and subroutines called in program
+
 contains
 
-! generates a random r given a
   real(dp) function r_sampler(mode, uniform_rand)
     real(dp) :: mode, uniform_rand, tolerance, r, r_new, diff, func
     integer :: j
@@ -63,20 +62,18 @@ program ChargeDensitySampler
   use sampler
   implicit none
   integer :: n_ele, i
-  real(dp) :: bohr_rad, atom_num, res
-  real(dp), dimension(:,:), allocatable :: ele_dist, ele_dist_rand
+  real(dp) :: bohr_rad, atom_num, res, radius, cos_theta, sin_theta, phi
+  real(dp), dimension(:,:), allocatable :: ele_dist, ele_dist_rand, ele_dist_cart
   real(dp), dimension(:), allocatable :: r_dist, r_histogram
 
-! assignments
   bohr_rad = 1.d0
   atom_num = 1.d0
-  n_ele = 50
+  n_ele = 500000
 
   allocate (ele_dist(3,n_ele))! arrays use Fortran ordering. (3,n_ele) would be better.
   allocate (ele_dist_rand(3,n_ele))
   call random_number(ele_dist_rand)
 
-! initial electron distribution
   do i = 1,n_ele
     ele_dist(1,i) = r_sampler(atom_num/bohr_rad, ele_dist_rand(1,i))
     ele_dist(2,i) = acos(cos_theta_sampler(ele_dist_rand(2,i)))
@@ -84,21 +81,30 @@ program ChargeDensitySampler
   end do
   deallocate (ele_dist_rand)
 
-! output
+! allocate (ele_dist_cart(3,n_ele))
+! do i = 1,n_ele
+!   radius = r_sampler(atom_num/bohr_rad, ele_dist_rand(1,i))
+!   cos_theta = cos_theta_sampler(ele_dist_rand(2,i))
+!   sin_theta = sqrt(1 - cos_theta**2)
+!   phi = phi_sampler(ele_dist_rand(3,i))
+!   ele_dist_cart(1,i) = radius*sin_theta*cos(phi)
+!   ele_dist_cart(2,i) = radius*sin_theta*sin(phi)
+!   ele_dist_cart(3,i) = radius*cos_theta
+! end do
+
 ! print "(3a20)", "r,", "theta,", "phi "
 ! do i = 1,n_ele
 !   print *, i
-!   print "(3f20.16)", ele_dist(1,i), ele_dist(2,i), ele_dist(3,i)
+!   print "(3f20.16)", ele_dist_cart(1,i), ele_dist_cart(2,i), ele_dist_cart(3,i)
 ! end do
 
   allocate (r_dist(n_ele))
   r_dist(:) = ele_dist(1,:)
   deallocate (ele_dist)
   res = 0.1
-  call histogram(r_dist,n_ele,res,r_histogram)
+  call histogram(r_dist,n_ele,res,r_histogram) ! histogram subroutine allocates r_histogram
   deallocate (r_dist)
 
-! Normalise this
   open (file="histogramdata.csv",unit=8,status="replace")
   do i = 1,size(r_histogram)
 !   print "(f20.16)", r_histogram(i)
@@ -106,4 +112,5 @@ program ChargeDensitySampler
   end do  
   close (8)
   deallocate (r_histogram) ! histogram subroutine allocates r_histogram
+! deallocate (ele_dist_cart)
 end program ChargeDensitySampler
