f2b4ed3269d9c0eae578d8b7537b050277c93205
MankyMeson <doylejohnson15@gmail.com>
2021-11-14 12:41:16 +0000

    list comprehension :)

--- plot.py
+++ plot.py
@@ -3,9 +3,7 @@ import matplotlib.pyplot as plt
 
 hist_data  = np.loadtxt("histogramdata.csv",dtype=float,delimiter=",",usecols=(1))
 bin_labels = np.loadtxt("histogramdata.csv",dtype=float,delimiter=",",usecols=(0))
-
-for i in range (len(bin_labels)):
-  bin_labels[i] = round(bin_labels[i], 2)
+bin_labels = [round(x,2) for x in bin_labels]
 
 plt.bar(bin_labels,hist_data,width=1.0,align='edge')
 plt.xlabel('radius, Hartree units')
