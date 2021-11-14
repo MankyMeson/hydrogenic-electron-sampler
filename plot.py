3f8bbaac0bc7628e609f9739bddf55f83de53fb6
MankyMeson <doylejohnson15@gmail.com>
2021-11-14 12:32:50 +0000

    Axes labels

--- plot.py
+++ plot.py
@@ -7,5 +7,7 @@ bin_labels = np.loadtxt("histogramdata.csv",dtype=float,delimiter=",",usecols=(0
 for i in range (len(bin_labels)):
   bin_labels[i] = round(bin_labels[i], 2)
 
-plt.bar(bin_labels,hist_data)
+plt.bar(bin_labels,hist_data,width=1.0,align='edge')
+plt.xlabel('radius, Hartree units')
+plt.ylabel('recovered probability (normalised frequency)')
 plt.show()
